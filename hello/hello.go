9682238692a91e40ca2f90413c7a6e4a3baab1ac
CarlChen <carl@greatbee.com>
2022-04-20 14:10:51 +0800

    add api

--- hello/hello.go
+++ hello/hello.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 	"log"
 
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
 	"example.com/greetings"
 	"golang.org/x/example/stringutil"
 	"rsc.io/quote"
 )
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func main() {
 	// fmt.Println("hello")
@@ -28,4 +45,12 @@ func main() {
 
 	fmt.Println("Hello End.")
 
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
